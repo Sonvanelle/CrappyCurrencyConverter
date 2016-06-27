314c7af145138e191441b013f147b66e2f167212
Charles Chan <s10173348@connect.np.edu.sg>
2016-06-27 15:38:14 +0800

    added a bunch of newlines

--- Converter.py
+++ Converter.py
@@ -2,7 +2,7 @@ import requests
 
 print("Simple Currency Converter")
 print("The source is: http://api.fixer.io/latest\n")
-base_currency = input("What is your base currency? Ex. HKD, EUR >> ")
+base_currency = input("\nWhat is your base currency? Ex. HKD, EUR >> ")
 url = 'http://api.fixer.io/latest?base=' + base_currency
 r = requests.get(url)
 print("Status code: ", r.status_code)
@@ -15,11 +15,11 @@ currencies = response_dict['rates']
 print("Currencies returned: ", len(currencies))
 
 while True:	
-	currency = input("What is the code of currency you want to convert to? Ex. USD, GBP, INR >> ")
+	currency = input("\nWhat is the code of currency you want to convert to? Ex. USD, GBP, INR >> ")
 	localcash = float(input("How much " + str(base_currency) + " do you have? >> "))
 
 	foreigncash = float(currencies.get(currency))
-	print("The conversion rate at this time is: ", foreigncash)
+	print("\nThe conversion rate at this time is: ", foreigncash)
 	conversion = localcash * foreigncash
 	print("You have: %.2f" %conversion, " in ", currency, ".")
 	
